9f89ff39bdda4f9b90849e7f5320bdfba7b69bcb
GonçaloCruz <a104346@alunos.uminho.pt>
2024-11-03 13:00:38 +0000

    Atualização

---
+++ agent.py
@@ -0,0 +1,48 @@
+import socket
+import sys
+import threading
+
+
+def udp_agent(address : str, port : str):
+    s : socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #s.bind((address,int(port)))
+    message : str = "adoro redes :):)"
+
+    s.sendto(message.encode("utf-8"), (address, int(port)))
+
+def tcp_agent(address : str, port : str):
+    s : socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect((address, int(port) +1))
+
+    message : str = "odeio redes"
+
+    s.send(message.encode('utf-8')) #passar para bytes a string que vai ser enviada
+
+    message = message+ ", mas tipo, muito"
+    
+    s.send(message.encode('utf-8'))
+
+
+def main():
+    address, port = sys.argv[1], sys.argv[2]
+
+    threads = [threading.Thread(target=udp_agent, args=(address,port)),
+               threading.Thread(target=tcp_agent, args=(address,port))]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    s : socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #s.bind((address,int(port)))
+    message : str = "adoro redes :):)"
+
+    s.sendto(message.encode("utf-8"), (address, int(port)))
+
+    
+
+if __name__ == "__main__":
+        main()
